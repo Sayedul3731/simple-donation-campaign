efa8b237a7846263fd1cf166ee396853b2429306
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-09-25 13:59:21 -0700

    when click on the Donate button then current card information add to the localStorage and at the same time show a toast 'thanks for donate'

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import Card from '../Card/Card';
 
 const Cards = ({cards}) => {
-    console.log(cards);
     return (
         <div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 my-10 mx-auto'>
--- src/components/SelectedCardDetails/SelectedCardDetails.jsx
+++ src/components/SelectedCardDetails/SelectedCardDetails.jsx
@@ -1,11 +1,32 @@
 import PropTypes from "prop-types"
 import "./SelectedCardDetails.css"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SelectedCardDetails = ({ card }) => {
-    const { price, description, title, image, color } = card;
+    const { id, price, description, title, image, color } = card;
+
+    const handleAddToDonate = () => {
+
+        const donateItemArray = []
+       const donationItems = JSON.parse(localStorage.getItem('donate'))
+       if(!donationItems){
+        donateItemArray.push(card)
+        localStorage.setItem('donate', JSON.stringify(donateItemArray))
+        toast("Thanks for donate")
+       }else{
+        const isExists = donationItems.find(donateCard => donateCard.id === id)
+        if(!isExists){
+            donateItemArray.push(...donationItems, card)
+            localStorage.setItem('donate', JSON.stringify(donateItemArray))
+        }else{
+           alert('already exists')
+        }
+
+       }
+    }
     return (
         <div>
-            <h1>Card Details</h1>
             <div className="relative flex w-full md:h-[80vh] my-10 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
                 <div className="relative h-[500px] w-full m-0 overflow-hidden rounded-none bg-transparent bg-clip-border text-gray-700 shadow-none">
                     <img className="h-full w-full object-cover"
@@ -14,7 +35,7 @@ const SelectedCardDetails = ({ card }) => {
                     />
                 </div>
                 <div className="w-full h-24 md:h-32 absolute mt-48 md:mt-56 lg:mt-[374px] cardOverlay">
-                    <button style={{ backgroundColor: color}} className="text-white py-2 px-4 font-semibold rounded-sm m-8 md:m-12" >Donate ${price}</button>
+                    <button style={{ backgroundColor: color}} onClick={handleAddToDonate} className="text-white py-2 px-4 font-semibold rounded-sm m-8 md:m-12" >Donate ${price}</button>
                 </div>
                 <div className="p-6">
                     <h4 className="block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
@@ -25,6 +46,7 @@ const SelectedCardDetails = ({ card }) => {
                     </p>
                 </div>
             </div>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
