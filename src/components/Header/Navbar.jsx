4a0ea6eadc65c1ad2ef9a7bc083c713c99f2abe6
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-09-25 10:17:01 -0700

    navbar responsive for all devices

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -4,41 +4,88 @@ import Logo from "../Logo/Logo";
 
 const Navbar = () => {
     return (
-        <nav className="flex justify-between items-center py-5">
-            <Logo></Logo>
-            <ul className="flex gap-5">
-                <li>
-                    <NavLink
-                        to="/"
-                        className={({ isActive, isPending }) =>
-                            isPending ? "pending" : isActive ? "text-red-600 font-medium underline" : ""
-                        }
-                    >
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/donation"
-                        className={({ isActive, isPending }) =>
-                            isPending ? "pending" : isActive ? "text-red-600 font-medium underline" : ""
-                        }
-                    >
-                        donation
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/statistics"
-                        className={({ isActive, isPending }) =>
-                            isPending ? "pending" : isActive ? "text-red-600 font-medium underline" : ""
-                        }
-                    >
-                        statistics
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li>
+                                <NavLink
+                                    to="/"
+                                    className={({ isActive, isPending }) =>
+                                        isPending ? "pending" : isActive ? "text-red-600 font-medium underline" : ""
+                                    }
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/donation"
+                                    className={({ isActive, isPending }) =>
+                                        isPending ? "pending" : isActive ? "text-red-600 font-medium underline" : ""
+                                    }
+                                >
+                                    donation
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/statistics"
+                                    className={({ isActive, isPending }) =>
+                                        isPending ? "pending" : isActive ? "text-red-600 font-medium underline" : ""
+                                    }
+                                >
+                                    statistics
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                    <Logo></Logo>
+                </div>
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li>
+                            <NavLink
+                                to="/"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "text-red-600 font-medium underline" : ""
+                                }
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/donation"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "text-red-600 font-medium underline" : ""
+                                }
+                            >
+                                donation
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/statistics"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "text-red-600 font-medium underline" : ""
+                                }
+                            >
+                                statistics
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
+
+
+        </div>
     );
 };
 
