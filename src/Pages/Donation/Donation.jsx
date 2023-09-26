90fbb23030139d550f01bf526a4b3f5bfa39fa61
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-09-25 17:48:30 -0700

    live link create by surge

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -24,12 +24,12 @@ const Donation = () => {
                 }
             </div>
             {
-                <div  className={cardsLength === cards.length ? 'hidden' : 'flex justify-center my-10'}><button onClick={() => setCardsLength(cards.length)}
+               cards.length >= 4 ? <div  className={cardsLength === cards.length ? 'hidden' : 'flex justify-center my-10'}><button onClick={() => setCardsLength(cards.length)}
                     className="middle none center rounded-lg bg-[#FF444A] py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                     data-ripple-light="true"
                 >
                     See All
-                </button></div> 
+                </button></div> : ''
             }
         </div>
     );
